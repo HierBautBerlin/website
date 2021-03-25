e6df98fd097690866415eab5f71073a7b7b5bbd2
Bodo Tasche <bodo@wannawork.de>
2021-03-25 12:53:30 +0100

    [#39] Bauprojekte Berliner Wirtschaft
    closes #39

---
+++ lib/hierbautberlin/importer/berlin_economy.ex
@@ -0,0 +1,67 @@
+defmodule Hierbautberlin.Importer.BerlinEconomy do
+  alias Hierbautberlin.GeoData
+
+  def import(http_connection \\ HTTPoison) do
+    {:ok, source} =
+      GeoData.upsert_source(%{
+        short_name: "BERLIN_ECONOMY",
+        name: "Neubauprojekte in Berlin",
+        url: "https://www.berlin.de/wirtschaft/bauprojekte/",
+        copyright: "Stadt Berlin"
+      })
+
+    items =
+      fetch_data(
+        http_connection,
+        "https://www.berlin.de/wirtschaft/bauprojekte/rubric.geojson?_rnd=448764"
+      )
+
+    Enum.map(items, fn item ->
+      attrs = to_geo_item(item)
+
+      {:ok, geo_item} = GeoData.upsert_geo_item(Map.merge(%{source_id: source.id}, attrs))
+
+      geo_item
+    end)
+  end
+
+  defp to_geo_item(item) do
+    point = parse_point(item["geometry"])
+    date = Timex.parse!(item["properties"]["modified"], "{RFC3339}")
+
+    %{
+      external_id: item["properties"]["url"],
+      title: item["properties"]["title"],
+      description: item["properties"]["description"],
+      url: item["properties"]["url"],
+      geo_point: point,
+      inserted_at: date,
+      updated_at: date
+    }
+  end
+
+  defp fetch_data(http_connection, url) do
+    response =
+      http_connection.get!(
+        url,
+        ["User-Agent": "hierbautberlin.de"],
+        timeout: 60_000,
+        recv_timeout: 60_000
+      )
+
+    if response.status_code != 200 do
+      []
+    else
+      Jason.decode!(response.body)["features"]
+    end
+  end
+
+  defp parse_point(%{"type" => "Point"} = point) do
+    [long, lat] = point["coordinates"]
+    %Geo.Point{coordinates: {long, lat}, srid: 3857}
+  end
+
+  defp parse_point(_) do
+    nil
+  end
+end
---
+++ test/hierbautberlin/importer/berlin_economy_test.exs
@@ -0,0 +1,88 @@
+defmodule Hierbautberlin.Importer.BerlinEconomyTest do
+  use Hierbautberlin.DataCase
+
+  alias Hierbautberlin.Importer.BerlinEconomy
+  alias Hierbautberlin.Repo
+  alias Hierbautberlin.GeoData
+
+  defmodule ImportMock do
+    def get!(
+          "https://www.berlin.de/wirtschaft/bauprojekte/rubric.geojson?_rnd=448764",
+          ["User-Agent": "hierbautberlin.de"],
+          timeout: 60_000,
+          recv_timeout: 60_000
+        ) do
+      {:ok, html} = File.read("./test/support/data/berlin_economy/geo.json")
+      %{body: html, headers: [], status_code: 200}
+    end
+  end
+
+  defmodule ImportUpdateMock do
+    def get!(
+          "https://www.berlin.de/wirtschaft/bauprojekte/rubric.geojson?_rnd=448764",
+          ["User-Agent": "hierbautberlin.de"],
+          timeout: 60_000,
+          recv_timeout: 60_000
+        ) do
+      {:ok, html} = File.read("./test/support/data/berlin_economy/geo_update.json")
+      %{body: html, headers: [], status_code: 200}
+    end
+  end
+
+  describe "import/1" do
+    test "basic import of infravelo data" do
+      result = BerlinEconomy.import(ImportMock)
+      assert length(result) == 24
+
+      first = List.first(result) |> Repo.preload(:source)
+
+      assert first.description ==
+               "Im Quartier Heidestraße entstehen neben dem bereits fertiggestellen QH Track drei weitere Gebäude für Wohnraum und Gewerbe. "
+
+      assert first.external_id ==
+               "https://www.berlin.de/wirtschaft/bauprojekte/6281921-4470362-mix-it-like-berlin-in-der-heidestrasse.html"
+
+      assert first.geo_geometry == nil
+
+      assert first.geo_point == %Geo.Point{
+               coordinates: {13.3681, 52.5295},
+               properties: %{},
+               srid: 3857
+             }
+
+      assert first.inserted_at == ~U[2021-03-24 13:51:44Z]
+      assert first.updated_at == ~U[2021-03-24 13:51:44Z]
+      assert first.participation_open == false
+      assert first.source.short_name == "BERLIN_ECONOMY"
+      assert first.state == nil
+      assert first.subtitle == nil
+      assert first.title == "Mix it like Berlin in der Heidestraße"
+
+      assert first.url ==
+               "https://www.berlin.de/wirtschaft/bauprojekte/6281921-4470362-mix-it-like-berlin-in-der-heidestrasse.html"
+    end
+
+    test "Updates an entry" do
+      first = ImportMock |> BerlinEconomy.import() |> List.first()
+
+      assert first.external_id ==
+               "https://www.berlin.de/wirtschaft/bauprojekte/6281921-4470362-mix-it-like-berlin-in-der-heidestrasse.html"
+
+      assert first.title == "Mix it like Berlin in der Heidestraße"
+      assert first.inserted_at == ~U[2021-03-24 13:51:44Z]
+      assert first.updated_at == ~U[2021-03-24 13:51:44Z]
+
+      second = ImportUpdateMock |> BerlinEconomy.import() |> List.first()
+      second = GeoData.get_geo_item!(second.id)
+
+      assert first.id == second.id
+
+      assert second.external_id ==
+               "https://www.berlin.de/wirtschaft/bauprojekte/6281921-4470362-mix-it-like-berlin-in-der-heidestrasse.html"
+
+      assert second.title == "Mix it like Berlin in der Heidestraße - Update"
+      assert second.inserted_at == ~U[2021-03-24 13:51:44Z]
+      assert second.updated_at == ~U[2021-05-24 13:51:44Z]
+    end
+  end
+end
