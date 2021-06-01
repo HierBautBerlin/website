609eb94acdc5f91c2b44fdb7571e1e25e4db84b2
Bodo Tasche <bitboxer@users.noreply.github.com>
2021-06-01 13:32:18 +0200

    [#52] Cronjob for imports (#98)
    closes #52

---
+++ lib/hierbautberlin/importer_cronjob.ex
@@ -0,0 +1,41 @@
+defmodule Hierbautberlin.ImporterCronjob do
+  use GenServer
+
+  def start_link(state) do
+    GenServer.start_link(__MODULE__, state)
+  end
+
+  def init(state) do
+    schedule_work()
+    {:ok, state}
+  end
+
+  def handle_info(:work, state) do
+    Hierbautberlin.Importer.import_all()
+    schedule_work()
+    {:noreply, state}
+  end
+
+  defp schedule_work() do
+    next_run_delay = calculate_next_cycle_delay(Timex.now())
+    Process.send_after(self(), :work, next_run_delay)
+  end
+
+  def calculate_next_cycle_delay(now) do
+    now
+    |> Timex.shift(hours: 1)
+    |> Timex.set(hour: 2, minute: 0, second: 0)
+    |> maybe_shift_a_day(now)
+    |> Timex.diff(now, :milliseconds)
+  end
+
+  defp maybe_shift_a_day(next_run, now) do
+    case Timex.before?(now, next_run) do
+      true ->
+        next_run
+
+      false ->
+        Timex.shift(next_run, days: 1)
+    end
+  end
+end
