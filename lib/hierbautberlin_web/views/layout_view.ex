55723895895c0f4c2fb1bc20d3417ed784bf0a85
Bodo Tasche <bitboxer@users.noreply.github.com>
2021-06-22 12:56:57 +0200

    [#162] Add openmetadata-headers (#163)
    closes #162

--- lib/hierbautberlin_web/views/layout_view.ex
+++ lib/hierbautberlin_web/views/layout_view.ex
@@ -1,3 +1,50 @@
 defmodule HierbautberlinWeb.LayoutView do
   use HierbautberlinWeb, :view
+
+  alias Phoenix.HTML
+  alias HierbautberlinWeb.Router.Helpers
+
+  def ogtags(conn) do
+    description = """
+    Was macht die Stadt in meinem Kiez? Warum ist hier eine Baustelle? Was wird demnächst gebaut?
+    Dich interessiert, was in deinem Umfeld passiert? Wo du dich beteiligen kannst? Eventuell
+    sogar mit einer E-Mail-Benachrichtigung, sobald etwas Neues gefunden wird? Dann ist
+    Hier Baut Berlin die Lösung.
+    """
+
+    image_url = Helpers.url(conn) <> "/images/hierbautberlin.png"
+
+    ogtags =
+      Map.merge(
+        %{
+          "og:title" => title(conn),
+          "og:description" => String.replace(description, "\n", " "),
+          "og:type" => "website",
+          "og:image" => image_url,
+          "og:url" => Helpers.url(conn) <> conn.request_path,
+          "twitter:card" => "summary",
+          "twitter:site" => "@hierbautberlin",
+          "twitter:description" => "Wir zeigen dir, was in Berlin passiert.",
+          "twitter:image" => String.replace(image_url, "http://", "https://")
+        },
+        conn.assigns[:ogtags] || %{}
+      )
+
+    render_metatags(ogtags)
+  end
+
+  defp render_metatags(tags) do
+    for {key, value} <- tags do
+      {:safe, safe_value} = HTML.html_escape(value)
+      raw("<meta property='#{key}' content='#{safe_value}'>\n")
+    end
+  end
+
+  defp title(%{assigns: %{page_title: title}}) do
+    "Hier Baut Berlin - #{title}"
+  end
+
+  defp title(_args) do
+    "Hier Baut Berlin"
+  end
 end
