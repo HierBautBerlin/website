f3b89f65155efcbf3242863463b238f433641e9a
Bodo Tasche <bodo@wannawork.de>
2021-07-30 12:46:23 +0200

    No more beta

--- config/prod.exs
+++ config/prod.exs
@@ -12,7 +12,7 @@ config :hierbautberlin, :environment, :prod
 # which you should run after static files are built and
 # before starting your production server.
 config :hierbautberlin, HierbautberlinWeb.Endpoint,
-  url: [host: "beta.hierbautberlin.de", port: 80],
+  url: [host: "hierbautberlin.de", port: 80],
   cache_static_manifest: "priv/static/cache_manifest.json"
 
 # Do not print debug messages in production
