83c8518f9d21444585a7d10ebb5dcf8543922e0c
Bodo Tasche <bitboxer@users.noreply.github.com>
2021-07-31 20:06:52 +0200

    [#288] Figure out why some items disappear in… (#289)
    …Strasse 6
    
    closes #288

--- assets/js/interactiveMap.ts
+++ assets/js/interactiveMap.ts
@@ -73,7 +73,7 @@ const updateMapItems = () => {
   mapItems.forEach((item:GeoItem) => {
     item.positions.forEach((position) => {
       if (position.point) {
-        mapFeatures[`point-${item.id}`] = {
+        mapFeatures[`point-${item.type}-${item.id}`] = {
           type: 'Feature',
           properties: {
             itemId: item.id,
@@ -88,7 +88,7 @@ const updateMapItems = () => {
 
       if (item.type === 'geo_item' && position.geometry) {
         if (isLineString(position.geometry)) {
-          mapFeatures[`linestring-${item.id}`] = {
+          mapFeatures[`linestring-${item.type}-${item.id}`] = {
             type: 'Feature',
             properties: {
               itemId: item.id,
@@ -99,7 +99,7 @@ const updateMapItems = () => {
             geometry: position.geometry,
           } as GeoJSON.Feature;
         } else {
-          mapFeatures[`polygon-${item.id}`] = {
+          mapFeatures[`polygon-${item.type}-${item.id}`] = {
             type: 'Feature',
             properties: {
               itemId: item.id,
